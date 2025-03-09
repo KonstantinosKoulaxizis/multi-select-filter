45590a32e549388fc93d9e94a6de455d716da13d
Kos Koulaxizis <koskoulaxizis@device-285.home>
2025-03-09 18:57:42 +0100

    feat: update zustand state with actions to fetch and update the selected items, update multy select component

--- src/components/multi-select-filter/MultiSelectList.tsx
+++ src/components/multi-select-filter/MultiSelectList.tsx
@@ -2,22 +2,48 @@ import { useQuery } from "@tanstack/react-query"
 import { useMultiSelectFilterStore } from "../../state"
 import { CheckboxList } from "../ui"
 import { getMockData } from "../../api/mockResponses"
+import { useShallow } from "zustand/shallow"
+import { useEffect } from "react"
 
+/**
+ * MultiSelectList Component
+ * 
+ * This component manages a multi-select list with search functionality. 
+ * It fetches and displays a list of selectable items while keeping track of selected ones.
+ * Selected items are shown separately, and the available options dynamically update based on user input.
+ */
 export const MultiSelectList = () => {
-    const searchQuery = useMultiSelectFilterStore((state) => state.searchQuery)
+    const { selectedItems, searchQuery, updateSelectedItems, fetchSelectedItems } = useMultiSelectFilterStore(
+        useShallow((state) => ({
+            searchQuery: state.searchQuery,
+            selectedItems: state.selectedItems,
+            updateSelectedItems: state.updateSelectedItems,
+            fetchSelectedItems: state.fetchSelectedItems,
+        })),
+    )
 
-    const { data } = useQuery<string[]>({ queryKey: ['multi-select-items', searchQuery], queryFn: () => getMockData(searchQuery) })
+    const { data, isLoading, error } = useQuery<string[]>({ queryKey: ['multi-select-items', searchQuery], queryFn: () => getMockData(searchQuery) })
 
-    if (!data) {
-        return null
-    }
+    const filteredData = data?.filter((item) => !selectedItems.includes(item)) || []
+
+    useEffect(() => {
+        fetchSelectedItems()
+    }, [fetchSelectedItems])
+
+    if (isLoading) return <div className="empty-list">Loading...</div>
+
+    if (error) return <div className="empty-list">{error.message}</div>
 
 
     return (
-        <div>
-            {/* <CheckboxList data={data?.pages}/> */}
-            <div className="list-container">
-                <CheckboxList data={data} onSelect={() => console.log('Select')} />
+        <div className="list-container">
+            {selectedItems.length > 0 && (
+                <div className="items-list">
+                <CheckboxList defaultChecked data={selectedItems} onSelect={updateSelectedItems} />
+                </div>
+            )}
+            <div className={`items-list ${selectedItems.length > 0 && 'list-without-margin'}`}>
+                <CheckboxList data={filteredData} onSelect={updateSelectedItems} />
             </div>
         </div>
     )
--- src/state/multiSelectFilterStore.ts
+++ src/state/multiSelectFilterStore.ts
@@ -1,13 +1,35 @@
 import { create } from "zustand";
 
+const SELECTED_ITEMS_KEY = 'selectedItems';
+
 interface MultiSelectFilterStoreState {
     searchQuery: string;
+    selectedItems: string[];
     setSearchQuery: (value: string) => void;
+    updateSelectedItems: (item: string) => void;
+    fetchSelectedItems: () => void;
 }
 
 export const useMultiSelectFilterStore = create<MultiSelectFilterStoreState>((set) => ({
     searchQuery: '',
+    selectedItems: [],
     setSearchQuery: (value: string) =>
         set({ searchQuery: value }),
+    updateSelectedItems: (item: string) => {
+        set((state) => {
+            const isSelected = state.selectedItems.includes(item);
+            const updatedItems = isSelected
+                ? state.selectedItems.filter((selectedItem) => selectedItem !== item)
+                : [...state.selectedItems, item];
 
+            localStorage.setItem(SELECTED_ITEMS_KEY, JSON.stringify(updatedItems));
+            return { selectedItems: updatedItems };
+        })
+    },
+    fetchSelectedItems: () => {
+        const selectedItems = localStorage.getItem(SELECTED_ITEMS_KEY);
+        if (selectedItems) {
+            set({ selectedItems: JSON.parse(selectedItems) });
+        }
+    }
 }));
